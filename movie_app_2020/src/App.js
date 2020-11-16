7ffb1748d9ac2545bac8a35cf7f0ef8a9c340833
scw3812 <dlffpraos@naver.com>
2020-11-16 16:56:05 +0900

    movie api 연결

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -12,7 +13,7 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     
     this.setState({ movies, isLoading: false });
   }
@@ -22,10 +23,24 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return (
-      <div>{isLoading ? 'Loading...' : 'We are Ready'}</div>
+      <div>
+        {isLoading 
+          ? 'Loading...' 
+          : movies.map((movie) => {
+              return (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image} />
+              );
+          })}
+      </div>
     );
   }
 }

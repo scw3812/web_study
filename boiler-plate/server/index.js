085955b3bafbdf493bfaaab088070f6f2a4f67be
scw3812 <dlffpraos@naver.com>
2020-12-01 21:04:08 +0900

    react 추가

--- boiler-plate/server/index.js
+++ boiler-plate/server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 
 const config = require('./config/key');
 
@@ -20,7 +21,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World?'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //회원가입 정보 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다
     const user = new User(req.body)
@@ -33,7 +34,7 @@ app.post('/register', (req, res) => {
     })
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //1. 요청 이메일 데이터베이스에서 찾기
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -58,6 +59,30 @@ app.post('/login', (req, res) => {
             })                        
         })
     })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastName: req.user.lastName,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => console.log(`Example app ${port}!`));

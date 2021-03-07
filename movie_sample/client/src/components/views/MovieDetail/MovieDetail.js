da828c084c47dfc1163b249f90eb335885163978
scw3812 <dlffpraos@naver.com>
2021-03-08 00:43:09 +0900

    movie detail

---
+++ movie_sample/client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import MainImage from '../LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
+function MovieDetail(props) {
+
+    let movieId = props.match.params.movieId
+
+    const [Movie, setMovie] = useState(null)
+
+    useEffect(() => {
+        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=ko-KR`;
+        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                setMovie(response)
+            })
+    }, [])
+
+    return (
+        <div>
+            {Movie &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+            }
+           <div style={{ width: '85%', margin: '3rem auto' }}>
+               {Movie &&
+                    <MovieInfo
+                        movie={Movie}
+                    />
+               }
+                <br/>
+                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                    <button> Toggle Actor View </button>    
+                </div>   
+            </div> 
+        </div>
+    )
+}
+
+export default MovieDetail

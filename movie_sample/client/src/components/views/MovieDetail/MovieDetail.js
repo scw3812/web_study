f2c32db8371764b66fea214bb29051d29c086378
scw3812 <dlffpraos@naver.com>
2021-03-13 17:27:36 +0900

    actor list

--- movie_sample/client/src/components/views/MovieDetail/MovieDetail.js
+++ movie_sample/client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,21 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd';
+
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
 
     const [Movie, setMovie] = useState(null)
+    const [Casts, setCasts] = useState(null)
+    const [ActorToggle, setActorToggle] = useState(false)
 
     useEffect(() => {
         let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=ko-KR`;
+        
         let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+        
         fetch(endpointInfo)
             .then(response => response.json())
             .then(response => {
                 setMovie(response)
             })
+
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                setCasts(response.cast)
+            })
     }, [])
+
+    const toogleActorView = e => {
+        setActorToggle(!ActorToggle)
+    }
 
     return (
         <div>
@@ -28,6 +45,11 @@ function MovieDetail(props) {
                 />
             }
            <div style={{ width: '85%', margin: '3rem auto' }}>
+               
+               <div style={{ display: 'flex', justifyContent:'flex-end' }}>
+                   <button onClick>Favorite</button>
+               </div>
+
                {Movie &&
                     <MovieInfo
                         movie={Movie}
@@ -35,8 +57,23 @@ function MovieDetail(props) {
                }
                 <br/>
                 <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                    <button> Toggle Actor View </button>    
-                </div>   
+                    <button onClick={toogleActorView}> Toggle Actor View </button>    
+                </div>
+
+                {ActorToggle &&
+                    <Row gutter={[16, 16]}>
+                        {Casts && Casts.map((cast, index) => (
+                            <React.Fragment key={index}>
+                                <GridCards 
+                                    image={cast.profile_path ?
+                                        `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}
+                                />
+                            </React.Fragment>
+                        ))}
+                    </Row>
+                }
+
             </div> 
         </div>
     )
--- movie_sample/client/src/components/views/commons/GridCards.js
+++ movie_sample/client/src/components/views/commons/GridCards.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { Col } from 'antd'
 
 function GridCards(props) {
-    return (
-        <Col lg={6} md={12} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <a href={`/movie/${props.movieId}`}>
-                    <img style={{ width: '100%', height: '320px' }}src={props.image} alt={props.movieName}/>
-                </a>
-            </div>
-        </Col>
-    )
+    
+    if (props.movieId) {
+        return (
+            <Col lg={6} md={12} xs={24}>
+                <div style={{ position: 'relative' }}>
+                    <a href={`/movie/${props.movieId}`}>
+                        <img style={{ width: '100%', height: '320px' }}src={props.image} alt={props.movieName}/>
+                    </a>
+                </div>
+            </Col>
+        )
+    } else {
+        return (
+            <Col lg={6} md={12} xs={24}>
+                <div style={{ position: 'relative' }}>
+                    <img style={{ width: '100%', height: '320px' }}src={props.image} alt={props.characterName}/>
+                </div>
+            </Col>
+        )
+    }
+    
 }
 
 export default GridCards

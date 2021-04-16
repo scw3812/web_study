cb0c7b4031bd41e011f0bd54e7e3d4c48752ba27
scw3812 <dlffpraos@naver.com>
2021-04-16 22:53:45 +0900

    webpack test

---
+++ es6_study/webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            
+        }],
+    },
+};

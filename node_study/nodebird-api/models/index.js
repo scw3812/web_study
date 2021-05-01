4b0da0b9d5bfad32292d9087e783e30fc6c36ea1
scw3812 <dlffpraos@naver.com>
2021-05-01 11:28:12 +0900

    initial nodebird-api

---
+++ node_study/nodebird-api/models/index.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const User = require('./user');
+const Post = require('./post');
+const Hashtag = require('./hashtag');
+const Domain = require('./domain');
+
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+db.Domain = Domain;
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+Domain.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
+Domain.associated(db);
+
+module.exports = db;
---
+++ node_study/nodebird-api/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { User, Domain } = require('../models');
+const { isLoggendIn } = require('./middlewares');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.user && req.user.id || null },
+      include: { model: Domain },
+    });
+    res.render('login', {
+      user,
+      domains: user && user.Domains,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/domain', isLoggendIn, async (req, res, next) => {
+  try {
+    await Domain.create({
+      userId: req.user.id,
+      host: req.body.host,
+      type: req.body.type,
+      clientSecret: uuidv4(),
+    });
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
